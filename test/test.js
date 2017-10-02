cd269605bb4670f4af8c1eb40d2514c67f0132be
Nicolas <nicolas.pestelle@hotmail.fr>
2017-10-02 11:40:03 +0200

    test non valide histoire 2

--- test/test.js
+++ test/test.js
@@ -6,3 +6,8 @@ LyngkTestCase.prototype.testHistoire1 = function () {
     var x = new Lyngk.Coordinates("A",1);
     assertTrue(x.est_Valide() == false);
 };
+
+LyngkTestCase.prototype.testHistoire2 = function () {
+  var x = new Lyngk.Coordinates("A",1);
+  assertTrue(x.nbTotalPositionValide() == 43);
+};
