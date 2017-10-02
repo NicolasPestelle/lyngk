c67438614fe0cf61cb45e7ee2a76531c32e73fb2
Nicolas <nicolas.pestelle@hotmail.fr>
2017-10-02 10:27:28 +0200

    ajout test histoire 1

--- test/test.js
+++ test/test.js
@@ -1,3 +1,7 @@
-'use strict';
+"use strict";
 
 var LyngkTestCase = TestCase("LyngkTestCase");
+
+LyngkTestCase.prototype.testHistoire1 = function () {
+    assertTrue(est_valide(A,1) == false);
+};
