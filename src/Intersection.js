9003004481aea75103fbb4a8f662a29e5bb0861b
Nicolas <nicolas.pestelle@hotmail.fr>
2017-10-09 11:01:23 +0200

    test histoire 8 reussi + code inter

--- src/Intersection.js
+++ src/Intersection.js
@@ -8,8 +8,21 @@ Lyngk.Intersection = function (c) {
 
 Lyngk.Intersection = function () {
     var private_etat = Lyngk.State.VACANT;
+    var private_color;
 
     this.get_etat = function () {
         return private_etat;
+    }
+
+    this.get_color = function () {
+        return private_color
+    }
+
+    this.placerPion = function (color) {
+        var color = color;
+        if (color == Lyngk.Color.BLUE ){
+            private_color = Lyngk.Color.BLUE;
+            private_etat = Lyngk.State.ONE_PIECE;
+        }
     }
 };
